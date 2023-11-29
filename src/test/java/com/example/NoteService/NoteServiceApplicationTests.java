83424d13e2341e7152d4672d1de616f271bec187
dmytrovolodovskyi <d.volodovskyi@gmail.com>
2023-11-29 23:26:04 +0100

    Revert "Revert "created note service""
    This reverts commit 746c5720666a683a45e0b201c94ca62b84648637.

---
+++ src/test/java/com/example/NoteService/NoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NoteService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
